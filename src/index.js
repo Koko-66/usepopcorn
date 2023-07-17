056c874559341b9e188829e23dd320d29dfaf4b4
Koko <pointmisser@gmail.com>
2023-07-17 22:34:11 +0100

    Add customization features to StarRating.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
-
 import StarRating from "./StarRating";
+
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="black" size="28" onSetRating={setMovieRating} />
+      <p>This movie is rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={"ssss"}
+      messages={["terrible", "bad", "ok", "good", "great"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={5} color="blue" size={23} className="test" />
+    <Test />
   </React.StrictMode>
 );
